5a9458cb1367effd6c4d01c526d57936187dc707
Yury Uvarov <uvarov.frontend@gmail.com>
2024-12-18 12:50:04 +0300

    update for vanilla-calendar-pro@3.0.3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,10 @@ const config: Config = {
   ],
   theme: {
     plugins: [],
+    screens: {
+      'xl': '1240px',
+    },
     extend: {
-      screens: {
-        'xl': '1240px',
-      },
       backgroundImage: {
         'arrow':
           'url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAyNCAyNCc+PHBhdGggZmlsbD0nI2ZmZicgZD0nTTEyIDE2Yy0uMyAwLS41LS4xLS43LS4zbC02LTZjLS40LS40LS40LTEgMC0xLjRzMS0uNCAxLjQgMGw1LjMgNS4zIDUuMy01LjNjLjQtLjQgMS0uNCAxLjQgMHMuNCAxIDAgMS40bC02IDZjLS4yLjItLjQuMy0uNy4zeicvPjwvc3ZnPg==")',
